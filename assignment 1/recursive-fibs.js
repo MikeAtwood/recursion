d090a7728c10dd0dbd605fe8deaf34949cf34fcf
Deadpool1 <atwood169@yahoo.com>
2023-05-11 16:09:47 -0500

    added in assignments

---
+++ assignment 1/recursive-fibs.js
@@ -0,0 +1,5 @@
+/*
+    Now write another method fibsRec which solves the same problem recursively. 
+    This can be done in just a couple of lines (or 1 if you’re crazy, but don’t
+    consider either of these lengths a requirement… just get it done).
+*/
