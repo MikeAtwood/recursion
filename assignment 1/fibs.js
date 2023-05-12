e88d3b2584f5a2dfcef622d100ee8169c751e27f
Deadpool1 <atwood169@yahoo.com>
2023-05-12 18:10:11 -0500

    completed fibs

--- assignment 1/fibs.js
+++ assignment 1/fibs.js
@@ -4,3 +4,31 @@
     this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
 */
 
+// console.log('hello world')
+
+function fibonacci(n) {
+    if (n <= 0) return 'Pick a number greater than 0'
+    const sequence = [0, 1]
+    for (let i = 2; i <= n; i++) {
+        const nextNumber = sequence[i - 1] + sequence[i - 2]
+        sequence.push(nextNumber)
+        }
+        return sequence
+    }
+console.log(fibonacci(5))
+
+// function fibonacci(n) {
+//     if (n <= 0) return 'Pick a number greater than 0'
+//     const sequence = []
+//     for (let i = 0; i < n; i++) {
+//         if (i < 2) {
+//             sequence.push(i)
+//         } else {
+//             const nextNumber = sequence[i - 1] + sequence[i - 2]
+//             sequence.push(nextNumber)
+//         }
+//     }
+//     return sequence
+// }
+
+// console.log(fibonacci(5))
