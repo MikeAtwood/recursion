d3c4863ab7e356253b07a4ba51e31ea76fc0aecb
Deadpool1 <atwood169@yahoo.com>
2023-05-23 13:41:47 -0500

    space complexity

---
+++ space-complexity/memory.js
@@ -0,0 +1,11 @@
+// O(1) - Constant Complexity
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+/*
+Here, no matter what the arguments we enter when we call the function, only two variables are 
+created. It doesn't change. Therefore, we can consider the space this takes is always O(1)
+*/
+
